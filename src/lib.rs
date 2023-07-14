9e0e21912759372d1063e08d3c7da84d9dc2cce7
Niclas Doege <niclas.doege@rwth-aachen.de>
2023-07-14 15:47:25 +0200

    initialized templates

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+mod utils;
+use wasm_bindgen::prelude::*;
+extern crate alloc;
+
+#[cfg(target_arch = "wasm32")]
+use lol_alloc::{FreeListAllocator, LockedAllocator};
+
+#[cfg(target_arch = "wasm32")]
+#[global_allocator]
+static ALLOCATOR: LockedAllocator<FreeListAllocator> = LockedAllocator::new(FreeListAllocator::new());
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, wasm-game-of-life!");
+}
